e4eba9898392d84b910511abd0ca182ad8d336af
Cocytus-Leon <smilingbaboon@126.com>
2022-01-20 11:12:27 +0800

    main

--- nn-SRCNN/main.py
+++ nn-SRCNN/main.py
@@ -6,9 +6,3 @@ import tifffile as tf
 import matplotlib.pyplot as plt
 print('Ready!')
 # %%
-im = tf.imread('../Data/10x/10x1.tif')
-plt.imshow(im)
-# %%
-im = tf.imread('../Data/20x/20x1.tif')
-plt.imshow(im)
-# %%
