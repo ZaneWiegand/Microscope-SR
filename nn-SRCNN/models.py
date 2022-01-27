daffc36844677fc1278b6106ad683c0f3a530398
Cocytus-Leon <smilingbaboon@126.com>
2022-01-27 22:24:42 +0800

    Update models.py

--- nn-SRCNN/models.py
+++ nn-SRCNN/models.py
@@ -7,6 +7,3 @@ class SRCNN(nn.Module):
     def __init__(self, num_channels=1):
         super(SRCNN, self).__init__()
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9//2)
-
-        nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1,
-                  groups=1, bias=True, padding_mode='zeros', device=None, dType=None)
