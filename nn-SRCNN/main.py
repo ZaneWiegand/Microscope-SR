bbc5f925f38dead5da56c58c4936ac3f6182fca2
Cocytus-Leon <smilingbaboon@126.com>
2022-01-27 22:24:25 +0800

    SRCNN

--- nn-SRCNN/main.py
+++ nn-SRCNN/main.py
@@ -1,12 +1,14 @@
 # %%
+import torch
+import torch.backends.cudnn as cudnn
 print('Ready!')
 # %%
 
 
 class Para(object):
-    train_file = 'train.h5'  # str
-    eval_file = 'eval.h5'  # str
-    outputs_dir = ''  # str
+    train_file = '/Users/zanewiegand/代码/python/Microscope-Super-Resolution/nn-SRCNN/train.h5'  # str
+    eval_file = '/Users/zanewiegand/代码/python/Microscope-Super-Resolution/nn-SRCNN/eval.h5'  # str
+    outputs_dir = '/Users/zanewiegand/代码/python/Microscope-Super-Resolution/nn-SRCNN'  # str
     lr = 1e-4  # float
     batch_size = 20  # int
     num_epochs = 100  # int
@@ -15,3 +17,10 @@ class Para(object):
 
 
 # %%
+args = Para()
+# %%
+cudnn.benchmark = True
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+torch.manual_seed(args.seed)
+# %%
+model = SRCNN().to(device)
