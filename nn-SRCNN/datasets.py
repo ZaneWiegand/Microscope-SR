3c36553c8bc1fdef8ef390650d4143bff4f48799
Cocytus-Leon <smilingbaboon@126.com>
2022-02-01 23:32:42 +0800

    update .h5

--- nn-SRCNN/datasets.py
+++ nn-SRCNN/datasets.py
@@ -9,3 +9,26 @@ class TrainDataset(Dataset):
     def __init__(self, h5_file):
         super(TrainDataset, self).__init__()
         self.h5_file = h5_file
+
+    def __getitem__(self, idx):
+        with h5py.File(self.h5_file, 'r') as f:
+            return np.expand_dims(f['lr'][idx]/255., 0), np.expand_dims(f['hr'][idx]/255., 0)
+
+    def __len__(self):
+        with h5py.File(self.h5_file, 'r') as f:
+            return len(f['lr'])
+# %%
+
+
+class EvalDataset(Dataset):
+    def __init__(self, h5_file):
+        super(EvalDataset, self).__init__()
+        self.h5_file = h5_file
+
+    def __getitem__(self, idx):
+        with h5py.File(self.h5_file, 'r') as f:
+            return np.expand_dims(f['lr'][str(idx)]/255., 0), np.expand_dims(f['hr'][str(idx)]/255., 0)
+
+    def __len__(self):
+        with h5py.File(self.h5_file, 'r') as f:
+            return len(f['lr'])
--- test/hdf5/view_data.py
+++ test/hdf5/view_data.py
@@ -20,7 +20,28 @@ print([key for key in pic_eval.keys()], "\n")
 print([key for key in pic_eval['hr'].keys()], "\n")
 print([key for key in pic_eval['lr'].keys()], "\n")
 # %%
-plt.imshow(pic_eval['hr']['20'][:])
+plt.imshow(pic_eval['hr']['40'][:])
 # %%
-plt.imshow(pic_eval['lr']['20'][:])
+plt.imshow(pic_eval['lr']['40'][:])
+# %%
+demo_train = h5py.File(
+    '/Users/zanewiegand/代码/python/Microscope-Super-Resolution/test/hdf5/91-image_x2.h5')
+demo_eval = h5py.File(
+    '/Users/zanewiegand/代码/python/Microscope-Super-Resolution/test/hdf5/Set5_x2.h5'
+)
+# %%
+print([key for key in demo_train.keys()], "\n")
+print([key for key in demo_eval.keys()], "\n")
+# %%
+print([key for key in demo_train.keys()], "\n")
+print([key for key in demo_eval['hr'].keys()], "\n")
+print([key for key in demo_eval['lr'].keys()], "\n")
+# %%
+plt.imshow(demo_train['hr'][22])
+# %%
+plt.imshow(demo_train['lr'][22])
+# %%
+plt.imshow(demo_eval['hr']['2'])
+# %%
+plt.imshow(demo_eval['lr']['2'])
 # %%
