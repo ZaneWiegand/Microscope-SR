328c1ad4e4e7cb7ad8338ba2ecd81a33db36ae80
Cocytus-Leon <smilingbaboon@126.com>
2022-02-14 00:05:00 +0800

    Update test.py

--- nn-SRCNN/test.py
+++ nn-SRCNN/test.py
@@ -1,3 +1,17 @@
 # %%
 import torch
+import torch.backends.cudnn as cudnn
+from models import SRCNN
 # %%
+
+
+class Para(object):
+    weights_file = ''
+    image_file = ''
+
+
+# %%
+arg = Para()
+cudnn.benchmark = True
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = SRCNN().to(device)
