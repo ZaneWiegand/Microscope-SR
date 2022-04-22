44d8e1b6760e8d9ffbee34ccdffc750cf4bd7837
Cocytus-Leon <smilingbaboon@126.com>
2022-04-22 18:35:01 +0800

    Update makedir.py

---
+++ Data-Preparation/makedir.py
@@ -0,0 +1,44 @@
+# %%
+import os
+# %%
+if not os.path.exists("../Data-Post-upsample/10x_train"):
+    os.makedirs("../Data-Post-upsample/10x_train")
+if not os.path.exists("../Data-Post-upsample/10x_train_syn"):
+    os.makedirs("../Data-Post-upsample/10x_train_syn")
+if not os.path.exists("../Data-Post-upsample/10x_eval"):
+    os.makedirs("../Data-Post-upsample/10x_eval")
+if not os.path.exists("../Data-Post-upsample/10x_eval_syn"):
+    os.makedirs("../Data-Post-upsample/10x_eval_syn")
+if not os.path.exists("../Data-Post-upsample/10x_predict"):
+    os.makedirs("../Data-Post-upsample/10x_predict")
+if not os.path.exists("../Data-Post-upsample/20x_train"):
+    os.makedirs("../Data-Post-upsample/20x_train")
+if not os.path.exists("../Data-Post-upsample/20x_train_syn"):
+    os.makedirs("../Data-Post-upsample/20x_train_syn")
+if not os.path.exists("../Data-Post-upsample/20x_eval"):
+    os.makedirs("../Data-Post-upsample/20x_eval")
+if not os.path.exists("../Data-Post-upsample/20x_eval_syn"):
+    os.makedirs("../Data-Post-upsample/20x_eval_syn")
+if not os.path.exists("../Data-Post-upsample/20x_truth"):
+    os.makedirs("../Data-Post-upsample/20x_truth")
+
+if not os.path.exists("../Data-Pre-upsample/10x_train"):
+    os.makedirs("../Data-Pre-upsample/10x_train")
+if not os.path.exists("../Data-Pre-upsample/10x_train_syn"):
+    os.makedirs("../Data-Pre-upsample/10x_train_syn")
+if not os.path.exists("../Data-Pre-upsample/10x_eval"):
+    os.makedirs("../Data-Pre-upsample/10x_eval")
+if not os.path.exists("../Data-Pre-upsample/10x_eval_syn"):
+    os.makedirs("../Data-Pre-upsample/10x_eval_syn")
+if not os.path.exists("../Data-Pre-upsample/10x_predict"):
+    os.makedirs("../Data-Pre-upsample/10x_predict")
+if not os.path.exists("../Data-Pre-upsample/20x_train"):
+    os.makedirs("../Data-Pre-upsample/20x_train")
+if not os.path.exists("../Data-Pre-upsample/20x_train_syn"):
+    os.makedirs("../Data-Pre-upsample/20x_train_syn")
+if not os.path.exists("../Data-Pre-upsample/20x_eval"):
+    os.makedirs("../Data-Pre-upsample/20x_eval")
+if not os.path.exists("../Data-Pre-upsample/20x_eval_syn"):
+    os.makedirs("../Data-Pre-upsample/20x_eval_syn")
+if not os.path.exists("../Data-Pre-upsample/20x_truth"):
+    os.makedirs("../Data-Pre-upsample/20x_truth")
