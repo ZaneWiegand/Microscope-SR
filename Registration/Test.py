29c43085eb69d5d3e0af627dae1142c4658dd139
Cocytus-Leon <smilingbaboon@126.com>
2022-04-30 12:23:42 +0800

    test

--- Registration/Test.py
+++ Registration/Test.py
@@ -37,7 +37,7 @@ pic20x = preprocess(pic20x, pre_threshold)
 # 插值pic1，变为原来的4倍
 pic10x_r, pic10x_c = pic10x.shape
 pic10x_ex = cv.resize(
-    pic10x, [pic10x_c * 2, pic10x_r * 2], interpolation=cv.INTER_LINEAR
+    pic10x, [pic10x_c * 2, pic10x_r * 2], interpolation=cv.INTER_CUBIC
 )
 method = cv.TM_SQDIFF_NORMED
 result = cv.matchTemplate(pic10x_ex, pic20x, method)
@@ -79,12 +79,12 @@ for i in range(layer):
     warp_img_layer[:, :, i] = warp_img  # i=0,是SIFT矫正的底层图
     target_img_layer[:, :, i] = target
     rmax_shift = cmax_shift = 1
-    while ~(rmax_shift < threshold and cmax_shift < threshold):
+    while ~(rmax_shift <= threshold and cmax_shift <= threshold):
         target = (target + warp_img + pic20x) / 3
         warp_block_stack = create_image_block_stack(
             warp_img, block_row, block_col)
         warp_img, rmax_shift, cmax_shift, rtmap, ctmap = blocks_registration(
-            warp_block_stack, target_block_stack, target, method, False
+            warp_block_stack, target_block_stack, target, method, True
         )
         print("第{}层位移: (r = {}, c = {})".format(i + 1, rmax_shift, cmax_shift))
         count += 1
@@ -101,16 +101,14 @@ for i in range(layer):
 
     target_block_stack = create_image_block_stack(target, block_row, block_col)
 # %%
-compare_unreg_reg(new, target, first_warp_img, global_method, 0, True)
-compare_unreg_reg(new, target, warp_img, global_method + "-PIPE", 0, True)
+compare_unreg_reg(new, target, first_warp_img, global_method, 0, False)
+compare_unreg_reg(new, target, warp_img, global_method + "-PIPE", 0, False)
 plt.figure(figsize=(10, 10))
-plt.imshow(20*np.log10(first_warp_img - warp_img).astype(np.uint8))
+# plt.imshow(20*np.log10(np.abs(first_warp_img - warp_img)).astype(np.uint8))
+plt.imshow(np.abs(first_warp_img - warp_img))
 plt.axis("off")
 plt.show()
-tf.imwrite(
-    "{}-PIPE.tif".format(global_method),
-    20*np.log10(first_warp_img - warp_img).astype(np.uint8),
-)
+# tf.imwrite("{}-PIPE.tif".format(global_method),np.abs(first_warp_img - warp_img))
 target = pic20x
 # %%
 # target = preprocess(target, 14)
@@ -145,28 +143,29 @@ for j, n in tup:
         )
 
 # %%
-if method == cv.INTER_CUBIC:
-    methods = "CUBIC"
-elif method == cv.INTER_LINEAR:
-    methods = "LINEAR"
-elif method == cv.INTER_NEAREST:
-    methods = "NEAREST"
-else:
-    methods = "ERROR"
-tf.imwrite(
-    "./Test-Result/test{}_pic_10x_{}_{}_{}_{}_{}divide.tif".format(
-        test_number, pre_threshold, global_method, methods, threshold, k + 1
-    ),
-    warp_img.astype(np.uint8),
-)
-tf.imwrite(
-    "./Test-Result/test{}_pic10x.tif".format(test_number),
-    pic10x_ex_cut.astype(np.uint8),
-)
-tf.imwrite(
-    "./Test-Result/test{}_pic_20x.tif".format(
-        test_number), pic20x.astype(np.uint8)
-)
+if False:
+    if method == cv.INTER_CUBIC:
+        methods = "CUBIC"
+    elif method == cv.INTER_LINEAR:
+        methods = "LINEAR"
+    elif method == cv.INTER_NEAREST:
+        methods = "NEAREST"
+    else:
+        methods = "ERROR"
+    tf.imwrite(
+        "./Test-Result/test{}_pic_10x_{}_{}_{}_{}_{}divide.tif".format(
+            test_number, pre_threshold, global_method, methods, threshold, k + 1
+        ),
+        warp_img.astype(np.uint8),
+    )
+    tf.imwrite(
+        "./Test-Result/test{}_pic10x.tif".format(test_number),
+        pic10x_ex_cut.astype(np.uint8),
+    )
+    tf.imwrite(
+        "./Test-Result/test{}_pic_20x.tif".format(
+            test_number), pic20x.astype(np.uint8)
+    )
 #! 荧光主程序结束
 #!############################################################################################
 # *###########################################################################################
